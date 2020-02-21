5d33d7187ad1414fd7a2186c2087f75ca8986567
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-21 03:47:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,7 +128,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-"Assembly"
-programmer_hash[:dennis_ritchie][:languages] << newly_added_hash
+
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 programmer_hash
 end
